2199ee508bc07f69a5a5c35677899db1f1001fb7
KatarzynaZiolkowska <64958610+KatarzynaZiolkowska@users.noreply.github.com>
2020-06-05 00:14:00 +0200

    cleaning
    To jest uporządkowany kod, wszystkie zmienne są po angielsku, funkcje ułożone w kolejności wykonywania. Dodałam także komentarze informujące co się dzieje w danym miejscu kodu.

--- blackjack_gui.py
+++ blackjack_gui.py
@@ -6,202 +6,201 @@ from tkinter import messagebox
 import tkinter as tk
 from PIL import Image , ImageTk
 
-okno = tk.Tk() #to open the window (ważne! Tk zamiast tk)
-okno.title("BLACKJACK")
-okno.geometry("800x500")
-okno.config(bg='green')
-gracz=[]
-krupier=[]
+player=[] #stowrzenie pustych list, do których program będzie dodawał kolejne karty
+croupier=[]
 
-los0 ="0"
-los01 ="0"
-los02 = "0"
-los03 ="0"
-los5 = "0"
-los6 = "0"
+players_card4 ="0"  #przypisanie początkowych nazw dobieranym kartom, tak by można było doliczyć "zerową ilość punktów" na wypadek niedobrania danej karty.
+players_card5 ="0"
+players_card6 ="0"
+croupier_card3 = "0"
+croupier_card4 = "0"
 
 player_total_points=0
 croupier_total_points = 0
 player_total_money=50
 
 
-def akcja_karta():
-    global img0
-    global los0
-    los0=los(deck)
-    img0 = tk.PhotoImage(file = los0)
-    img0 = img0.subsample(6)
-    label = tk.Label(okno, image=img0)
-    label.place(x=250,y=300)
-    gracz.append(los0)
-    przycisk1.config(state = tk.DISABLED)
-    przycisk2.config(state = tk.DISABLED)
-    global przycisk1_1
-    global przycisk2_1
-    points(p_litera(los0))
-    przycisk1_1 = Button(okno, text = "Dobierz kartę", bg="black", fg="white", command = akcja_karta2)
-    przycisk1_1.place(x=0,y=50)
-    przycisk2_1 = Button(okno, text = "Nie dobieraj", bg="black", fg="white", command= akcja_karta_k1)
-    przycisk2_1.place(x=0,y=80)
+def deal_of_cards():    #Ta funkcja zadziała po naciśnięciu przycisku:Zagraj i rozda po dwie karty graczowi i krupierowi(jedna karta krupiera będzie zakryta).
 
-def akcja_karta2():
-    global img01
-    global los01
-    los01=los(deck)
-    img01 = tk.PhotoImage(file = los01)
-    img01 = img01.subsample(6)
-    label = tk.Label(okno, image=img01)
-    label.place(x=325,y=300)
-    gracz.append(los01)
-    przycisk1_1.config(state = tk.DISABLED)
-    przycisk2_1.config(state = tk.DISABLED)
-    global przycisk1_2
-    global przycisk2_2
-    points(p_litera(los01))
-    przycisk1_2 = Button(okno, text = "Dobierz kartę", bg="black", fg="white", command = akcja_karta3)
-    przycisk1_2.place(x=0,y=50)
-    przycisk2_2 = Button(okno, text = "Nie dobieraj", bg="black", fg="white", command= akcja_karta_k1)
-    przycisk2_2.place(x=0,y=80)
+    global image1
+    global players_card1
+    players_card1=los(deck)
+    image1 = tk.PhotoImage(file = players_card1)
+    image1 = image1.subsample(6)
+    label = tk.Label(window, image=image1)
+    label.place(x=100,y=300)
+    player.append(players_card1)
 
+    global image2
+    global players_card2
+    players_card2=los(deck)
+    image2 = tk.PhotoImage(file = players_card2)
+    image2 = image2.subsample(6)
+    label = tk.Label(window, image=image2)
+    label.place(x=175,y=300)
+    player.append(players_card2)
 
+    global image3
+    global croupier_card1
+    croupier_card1=los(deck)
+    image3 = tk.PhotoImage(file = croupier_card1)
+    image3 = image3.subsample(6)
+    label = tk.Label(window, image=image3)
+    label.place(x=375,y=60)
+    croupier.append(croupier_card1)
 
-def akcja_karta3():
-    global img02
-    global los02
-    los02=los(deck)
-    img02 = tk.PhotoImage(file = los02)
-    img02 = img02.subsample(6)
-    label = tk.Label(okno, image=img01)
-    label.place(x=400,y=300)
-    gracz.append(los02)
-    przycisk1_2.config(state = tk.DISABLED)
-    przycisk2_2.config(state = tk.DISABLED)
-    global przycisk1_3
-    global przycisk2_3
-    points(p_litera(los02))
-    przycisk1_3 = Button(okno, text = "Dobierz kartę", bg="black", fg="white", command = akcja_karta4)
-    przycisk1_3.place(x=0,y=50)
-    przycisk2_3 = Button(okno, text = "Nie dobieraj", bg="black", fg="white", command= akcja_karta_k1)
-    przycisk2_3.place(x=0,y=80)
-
-
-def akcja_karta4():
-    global img03
-    global los03
-    los03=los(deck)
-    img03 = tk.PhotoImage(file = los03)
-    img03 = img03.subsample(6)
-    label = tk.Label(okno, image=img03)
-    label.place(x=475,y=300)
-    gracz.append(los03)
-    przycisk1_3.config(state = tk.DISABLED)
-    przycisk2_3.config(state = tk.DISABLED)
-    points(p_litera(los03))
-    akcja_karta_k1()
-
-
-def akcja_karta_k1():
-    global img4
-    global los4
-    los4=los(deck)
-    img4 = tk.PhotoImage(file = los4)
-    img4 = img4.subsample(6)
-    label = tk.Label(okno, image=img4)
+    global image4
+    image4 = tk.PhotoImage(file = "purple_back.png")
+    image4 = image4.subsample(6)
+    label = tk.Label(window, image=image4)
     label.place(x=450,y=60)
-    krupier.append(los4)
-    points2(p_litera(los4))
-    if croupier_total_points < 17:
-        akcja_karta_k2()
-    elif croupier_total_points >= 17:
-        liczenie_punktow()
+    play_button.config(state = tk.DISABLED)
 
-def akcja_karta_k2():
-    global img5
-    global los5
-    los5=los(deck)
-    img5 = tk.PhotoImage(file = los5)
-    img5 = img5.subsample(6)
-    label = tk.Label(okno, image=img5)
+    global button1 #Tu powstają przyciski które pozwalają graczowi na podjęcie decyzji czy dobiera kartę, czy też nie dobiera
+    global button2
+    button1 = Button(window, text = "Dobierz kartę", bg="black", fg="white", command = picking_card_player1)
+    button1.place(x=0,y=50)
+    button2 = Button(window, text = "Nie dobieraj", bg="black", fg="white", command= picking_card_croupier1)
+    button2.place(x=0,y=80)
+    points(first_letter(players_card1))
+    points(first_letter(players_card2))
+    points2(first_letter(croupier_card1))
+
+ #Teraz rozpisane zostaną funkcje pozwalające graczowi na dobieranie kolejnych kart i doliczanie ich wartości do łącznej liczby punktów gracza
+def picking_card_player1(): # To jest funkcja, która dobiera pierwszą kartę dla gracza
+    global image7
+    global players_card3
+    players_card3=los(deck)
+    image7 = tk.PhotoImage(file = players_card3)
+    image7 = image7.subsample(6)
+    label = tk.Label(window, image=image7)
+    label.place(x=250,y=300)
+    player.append(players_card3)
+    button1.config(state = tk.DISABLED)
+    button2.config(state = tk.DISABLED)
+    global button1_1
+    global button2_1
+    points(first_letter(players_card3))
+    button1_1 = Button(window, text = "Dobierz kartę", bg="black", fg="white", command = picking_card_player2)
+    button1_1.place(x=0,y=50)
+    button2_1 = Button(window, text = "Nie dobieraj", bg="black", fg="white", command= picking_card_croupier1)
+    button2_1.place(x=0,y=80)
+
+def picking_card_player2():  # To jest funkcja, która dobiera drugą kartę dla gracza
+    global image8
+    global players_card4
+    players_card4=los(deck)
+    image8= tk.PhotoImage(file = players_card4)
+    image8 = image8.subsample(6)
+    label = tk.Label(window, image=image8)
+    label.place(x=325,y=300)
+    player.append(players_card4)
+    button1_1.config(state = tk.DISABLED)
+    button2_1.config(state = tk.DISABLED)
+    global button1_2
+    global button2_2
+    points(first_letter(players_card4))
+    button1_2 = Button(window, text = "Dobierz kartę", bg="black", fg="white", command = picking_card_player3)
+    button1_2.place(x=0,y=50)
+    button2_2 = Button(window, text = "Nie dobieraj", bg="black", fg="white", command= picking_card_croupier1)
+    button2_2.place(x=0,y=80)
+
+
+def picking_card_player3():   # To jest funkcja, która dobiera trzecią kartę dla gracza
+    global image9
+    global players_card5
+    players_card5=los(deck)
+    image9 = tk.PhotoImage(file = players_card5)
+    image9 = image9.subsample(6)
+    label = tk.Label(window, image=image9)
+    label.place(x=400,y=300)
+    player.append(players_card5)
+    button1_2.config(state = tk.DISABLED)
+    button2_2.config(state = tk.DISABLED)
+    global button1_3
+    global button2_3
+    points(first_letter(players_card5))
+    button1_3 = Button(window, text = "Dobierz kartę", bg="black", fg="white", command = picking_card_player4)
+    button1_3.place(x=0,y=50)
+    button2_3 = Button(window, text = "Nie dobieraj", bg="black", fg="white", command= picking_card_croupier1)
+    button2_3.place(x=0,y=80)
+
+
+def picking_card_player4():  # To jest funkcja, która dobiera czwartą kartę dla gracza
+    global image10
+    global players_card6
+    players_card6=los(deck)
+    image10 = tk.PhotoImage(file = players_card6)
+    image10 = image10.subsample(6)
+    label = tk.Label(window, image=image10)
+    label.place(x=475,y=300)
+    player.append(players_card6)
+    button1_3.config(state = tk.DISABLED)
+    button2_3.config(state = tk.DISABLED)
+    points(first_letter(players_card6))
+    picking_card_croupier1()
+
+
+
+
+ #Teraz rozpisane zostaną funkcje, które będą dobierać karty dla krupiera, jeśli suma jego punktów jest <17. W przeciwnym razie program przejdzie do liczenia punktów.
+
+def picking_card_croupier1():  #To jest funkcja, która odkrywa zakrytą kartę krupiera.
+    global image5
+    global croupier_card2
+    croupier_card2=los(deck)
+    image5 = tk.PhotoImage(file = croupier_card2)
+    image5 = image5.subsample(6)
+    label = tk.Label(window, image=image5)
+    label.place(x=450,y=60)
+    croupier.append(croupier_card2)
+    points2(first_letter(croupier_card2))
+    if croupier_total_points < 17:
+        picking_card_croupier2()
+    elif croupier_total_points >= 17:
+        points_counting()
+
+def picking_card_croupier2(): # To jest funnkcja, która dobiera pierwszą kartę krupiera.
+    global image4
+    global croupier_card3
+    croupier_card3=los(deck)
+    image4 = tk.PhotoImage(file = croupier_card3)
+    image4 = image4.subsample(6)
+    label = tk.Label(window, image=image4)
     label.place(x=525,y=60)
-    krupier.append(los5)
-    points2(p_litera(los5))
+    croupier.append(croupier_card3)
+    points2(first_letter(croupier_card3))
     if croupier_total_points < 17:
-        akcja_karta_k3()
+        picking_card_croupier3()
     elif croupier_total_points >= 17:
-        liczenie_punktow()
+        points_counting()
 
-def akcja_karta_k3():
-    global img6
-    global los6
-    los6=los(deck)
-    img6 = tk.PhotoImage(file = los6)
-    img6 = img6.subsample(6)
-    label = tk.Label(okno, image=img6)
+def picking_card_croupier3():  #To jest funkcja, która dobiera drugą kartę, krupiera.
+    global image6
+    global croupier_card4
+    croupier_card4=los(deck)
+    image6 = tk.PhotoImage(file = croupier_card4)
+    image6 = image6.subsample(6)
+    label = tk.Label(window, image=image6)
     label.place(x=600,y=60)
-    krupier.append(los6)
-    points2(p_litera(los6))
-    liczenie_punktow()
+    croupier.append(croupier_card4)
+    points2(first_letter(croupier_card4))
+    points_counting()
 
-def liczenie_punktow():
+
+
+
+def points_counting(): #Ta funkcja pozwala na podsumowanie otrzymanych punktów i wyświetlenie wyniku
     print(player_total_points, croupier_total_points)
     if player_total_points >21 or croupier_total_points > player_total_points and croupier_total_points <21:
-        wynik = Label(okno, text = "Przegrałeś.", bg="blue", fg="black", font="none 15 bold")
-        wynik.place(x =550, y = 300)
+        result = Label(window, text = "Przegrałeś.", bg="blue", fg="black", font="none 15 bold")
+        result.place(x =550, y = 300)
     elif player_total_points == 21 and croupier_total_points !=21:
-         wynik = Label(okno, text = "Masz blakcjacka! Gratulacje!", bg="blue", fg="black", font="none 15 bold")
-         wynik.place(x =550, y = 300)
+         result = Label(window, text = "Masz blakcjacka! Gratulacje!", bg="blue", fg="black", font="none 15 bold")
+         result.place(x =550, y = 300)
     elif croupier_total_points >21 or player_total_points > croupier_total_points and player_total_points <21:
-         wynik = Label(okno, text = "Wygrałeś! Gratulacje!", bg="blue", fg="black", font="none 15 bold")
-         wynik.place(x =550, y = 300)
-
-def akcja_przycisk_1():
-
-    global img
-    global los1
-    los1=los(deck)
-    img = tk.PhotoImage(file = los1)
-    img = img.subsample(6)
-    label = tk.Label(okno, image=img)
-    label.place(x=100,y=300)
-    gracz.append(los1)
-    global img2
-    global los2
-    los2=los(deck)
-    img2 = tk.PhotoImage(file = los2)
-    img2 = img2.subsample(6)
-    label = tk.Label(okno, image=img2)
-    label.place(x=175,y=300)
-    gracz.append(los2)
-    global img3
-    global los3
-    los3=los(deck)
-    img3 = tk.PhotoImage(file = los3)
-    img3 = img3.subsample(6)
-    label = tk.Label(okno, image=img3)
-    label.place(x=375,y=60)
-    krupier.append(los3)
-    global img5
-    img5 = tk.PhotoImage(file = "purple_back.png")
-    img5 = img5.subsample(6)
-    label = tk.Label(okno, image=img5)
-    label.place(x=450,y=60)
-    przycisk_1.config(state = tk.DISABLED)
-    global przycisk1
-    global przycisk2
-    przycisk1 = Button(okno, text = "Dobierz kartę", bg="black", fg="white", command = akcja_karta)
-    przycisk1.place(x=0,y=50)
-    przycisk2 = Button(okno, text = "Nie dobieraj", bg="black", fg="white", command= akcja_karta_k1)
-    przycisk2.place(x=0,y=80)
-    points(p_litera(los1))
-    points(p_litera(los2))
-    points2(p_litera(los3))
-
-
-def akcja_przycisk_2():
-    messagebox.showinfo("Zasady","Celem gry jest pokonać krupiera poprzez uzyskanie sumy jak najbliższej 21 punktów w kartach, jednak nie przekraczając 21. Gracz stawia zakład na specjalnym stole używając żetonów. Następnie gracz i krupier dostają po dwie karty. Obydwie karty gracza są odkryte, natomiast tylko jedna karta krupiera jest pokazana graczowi\nPUNKTACJA KART:\nKarty od dwójki do dziesiątki mają wartość równą numerowi karty.\nWalet, Dama i Król mają wartość równą 10 punktów.\nAs ma wartość równą 1 lub 11, w zależności co jest lepsze dla gracza.\nMOŻLIWOŚCI RUCHU\nW swojej turze możesz:\n1) Dobrać kartę (hit)\n2) Nie dobierać kart (stand)\n3) Podwoić stawkę (double down)\n4) Rozdwoić karty (split)")
-
-def akcja_przycisk_4():
-    messagebox.showinfo("Autorki programu","Łzy, krew i pot wylały:\nAnna Cichocka\nAnna Kołodziejczyk\nKatarzyna Ziółkowska\nSylwia Leśniewska")
+         result = Label(window, text = "Wygrałeś! Gratulacje!", bg="blue", fg="black", font="none 15 bold")
+         result.place(x =550, y = 300)
 
 
 #tworzymy listę wszystkich plików w folderze
@@ -211,87 +210,93 @@ deck.remove("blackjack_gui.py")
 deck.remove("purple_back.png")
 # deck.remove("gjuu.py")
 def los(deck):
-    wylosowana=random.choice(deck)
-    return wylosowana
-
+    random_card=random.choice(deck)
+    return random_card
 #używamy pierwszej litery pliku do rozpoznania karty (dlatego też dobrze by było, żeby w folderze znajdowały)
-def p_litera(wylosowana):
-    symbol_wylosowana=wylosowana[0]
-    return symbol_wylosowana
+def first_letter(random_card):
+    random_card_symbol=random_card[0]
+    return random_card_symbol
 
-przycisk_1=Button(okno, text="Zagraj", bg="red", fg="white", font="none 15 bold", command=akcja_przycisk_1,)
-przycisk_1.place(x=0,y=0)
-
-przycisk_2=Button(okno, text="Zasady", bg="blue", fg="white", font="none 15 bold", command=akcja_przycisk_2)
-przycisk_2.place(x=75,y=0)
-
-przycisk_3=Button(okno, text="Wyjdź z programu", bg="yellow", fg="black", font="none 15 bold", command=okno.quit)
-przycisk_3.place(x=160,y=0)
-
-przycisk_4=Button(okno, text="Autorki", bg="orange", fg="black", font="none 15 bold", command=akcja_przycisk_4)
-przycisk_4.place(x=348,y=0)
-
-
-
-def points(karta):
+#Funkcje przypisujące wartości poszczególnym kartom.
+def points(card):
     global player_total_points
-    if karta=="2":
+    if card=="2":
         player_total_points += 2
-    elif karta=="3":
+    elif card=="3":
         player_total_points += 3
-    elif karta=="4":
+    elif card=="4":
         player_total_points += 4
-    elif karta=="5":
+    elif card=="5":
         player_total_points += 5
-    elif karta=="6":
+    elif card=="6":
         player_total_points += 6
-    elif karta=="7":
+    elif card=="7":
         player_total_points += 7
-    elif karta=="8":
+    elif card=="8":
         player_total_points += 8
-    elif karta=="9":
+    elif card=="9":
         player_total_points +=9
-    elif karta=="A":
+    elif card=="A":
         player_total_points += 11
         if player_total_points > 21:
             player_total_points = player_total_points -10
         else:
             player_total_points = player_total_points
-    elif karta=="J" or karta=="K" or karta=="Q" or karta=="1":
+    elif card=="J" or card=="K" or card=="Q" or card=="1":
         player_total_points +=10
 
     return player_total_points
 
 
-def points2(karta):
+def points2(card):
     global croupier_total_points
-    if karta=="2":
+    if card=="2":
         croupier_total_points += 2
-    elif karta=="3":
+    elif card=="3":
         croupier_total_points += 3
-    elif karta=="4":
+    elif card=="4":
         croupier_total_points += 4
-    elif karta=="5":
+    elif card=="5":
         croupier_total_points += 5
-    elif karta=="6":
+    elif card=="6":
         croupier_total_points += 6
-    elif karta=="7":
+    elif card=="7":
         croupier_total_points += 7
-    elif karta=="8":
+    elif card=="8":
         croupier_total_points += 8
-    elif karta=="9":
+    elif card=="9":
         croupier_total_points +=9
-    elif karta=="A":
+    elif card=="A":
         croupier_total_points += 11
         if croupier_total_points > 21:
             croupier_total_points = croupier_total_points -10
         else:
             croupier_total_points = croupier_total_points
-    elif karta=="J" or karta=="K" or karta=="Q" or karta=="1":
+    elif card=="J" or card=="K" or card=="Q" or card=="1":
         croupier_total_points +=10
 
     return croupier_total_points
 
+#Funkcje dla przycisków z informacjami
+def rules_info():
+    messagebox.showinfo("Zasady","Celem gry jest pokonać krupiera poprzez uzyskanie sumy jak najbliższej 21 punktów, jednak nie przekraczając 21. Gracz stawia zakład na specjalnym stole używając żetonów. Następnie gracz i krupier dostają po dwie karty. Obydwie karty gracza są odkryte, natomiast tylko jedna card krupiera jest pokazana graczowi\nPUNKTACJA KART:\nKarty od dwójki do dziesiątki mają wartość równą numerowi karty.\nWalet, Dama i Król mają wartość równą 10 punktów.\nAs ma wartość równą 1 lub 11, w zależności co jest lepsze dla gracza.\nMOŻLIWOŚCI RUCHU\nW swojej turze możesz:\n1) Dobrać kartę (hit)\n2) Nie dobierać kart (stand)\n3) Podwoić stawkę (double down)\n4) Rozdwoić karty (split)")
 
-okno.mainloop()
+def authors_info():
+    messagebox.showinfo("Autorki programu","Łzy, krew i pot wylały:\nAnna Cichocka\nAnna Kołodziejczyk\nKatarzyna Ziółkowska\nSylwia Leśniewska")
 
+
+# Uruchamianie przycisków
+play_button=Button(window, text="Zagraj", bg="red", fg="white", font="none 15 bold", command= deal_of_cards,)
+play_button.place(x=0,y=0)
+
+rules_button=Button(window, text="Zasady", bg="blue", fg="white", font="none 15 bold", command= rules_info)
+rules_button.place(x=75,y=0)
+
+quit_button=Button(window, text="Wyjdź z programu", bg="yellow", fg="black", font="none 15 bold", command=window.quit)
+quit_button.place(x=160,y=0)
+
+authors_button=Button(window, text="Autorki", bg="orange", fg="black", font="none 15 bold", command=authors_info)
+authors_button.place(x=348,y=0)
+
+
+window.mainloop()
